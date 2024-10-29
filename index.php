5a8799628e4e8dc3dfbab137ceb2f38ae1276b8e
jrivmon <jrivmon1909@g.educaand.es>
2024-10-29 15:45:24 +0100

    hola

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web de Juan José Rivera Montes</title>
+</head>
+<body>
+    <h1>Juan José Rivera Montes</h1>
+    <?php
+    
+        date_default_timezone_get('Europe/Madrid');
+        echo date('H:i:s');
+    ?>
+    <div id="relojCliente"></div>
+    
+    <form action="insertar.php" method="POST">
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="nombre" id="nombre" required>
+        <br>
+        <label for="edad">Edad:</label>
+        <input type="number" name="edad" id="edad" min="1" required>
+        <br>
+        <input type="submit" value="enviar">
+        
+    </form>
+    <script src="js/reloj.js"></script>
+</body>
+</html>
